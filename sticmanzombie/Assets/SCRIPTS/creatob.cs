7a3e244ed3de3d25e5dc401531424a6a79afe567
0000apa0000 <99506016+0000apa0000@users.noreply.github.com>
2022-02-25 19:37:35 +0330

    1400-11-5

---
+++ sticmanzombie/Assets/SCRIPTS/creatob.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creatob : MonoBehaviour
+{
+     public GameObject[] OB  = new GameObject[2];
+    Transform posone, postwo;
+
+
+    float randon ;
+    int randominstan;
+    void Awake()
+    {
+       posone= GameObject.Find("leftwallforOB").transform;
+        postwo = GameObject.Find("rightwallforOB").transform;
+       
+        for (int i = 0; i < 20; i++)
+        {
+
+            randominstan = Random.Range(0,2);
+            randon = Random.Range(posone.position.x + 15f, postwo.position.x - 15f);
+
+            Instantiate(OB[randominstan] , new Vector3(randon , OB[randominstan].transform.position.y , 1f) , Quaternion.identity);
+
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+     
+    }
+}
---
+++ sticmanzombie/Assets/SCRIPTS/obrepeat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obrepeat : MonoBehaviour
+{
+    public float mindistance;
+    public float maxdistance;
+    float aa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        aa = Random.Range(mindistance, maxdistance);
+
+        if (col.CompareTag("OB"));
+        {
+
+
+            col.transform.position = new Vector2(col.transform.position.x + aa, col.transform.position.y);
+
+
+        }
+    }
+}
